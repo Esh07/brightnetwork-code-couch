e122aaa177947a8107b64dd63ed4105492b2d6ed
Esh <32418603+Esh07@users.noreply.github.com>
2024-09-05 15:20:03 +0100

    add: Final code to git

---
+++ main.js
@@ -0,0 +1,80 @@
+
+async function fetchImages() {
+    try {
+        const response = await fetch(
+            "https://picsum.photos/v2/list?page=2&limit=20"
+        );
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const images = await response.json();
+        const gallery = document.getElementById("gallery");
+
+        // Clear existing content
+        gallery.innerHTML = "";
+
+        images.forEach((image, index) => {
+            const galleryItem = document.createElement("div");
+            galleryItem.className = "gallery-item";
+
+            const img = document.createElement("img");
+            img.src = image.download_url;
+            img.alt = `Image by ${image.author}`;
+            img.loading = "lazy";
+
+            const overlay = document.createElement("div");
+            overlay.className = "overlay";
+            overlay.innerHTML = `
+          <div class="overlay-content">
+            <span class="location"> <i class="fa-regular fa-location-dot"></i> Location, India</span>
+             <div class="overlay-content-meta">
+             <div class="overlay-content-meta-content">
+               <div class="auther">
+                  <i class="fa-regular fa-user "></i>
+                  <h3>${image.author || "Unknown Author"}</h3>
+                 </div>
+                <span class="date">${new Date().toLocaleDateString()}</span>
+            </div>
+                 </div>
+            
+          </div>
+        `;
+
+            galleryItem.appendChild(img);
+            galleryItem.appendChild(overlay);
+            gallery.appendChild(galleryItem);
+
+        });
+    } catch (error) {
+        console.error("Error fetching images:", error);
+    }
+}
+
+// Initialise the image fetch and slider setup
+document.addEventListener("DOMContentLoaded", fetchImages);
+
+// Function to generate a random color
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let baseColor = "#";
+    for (let i = 0; i < 6; i++) {
+        baseColor += letters[Math.floor(Math.random() * 16)];
+    }
+    return {
+        baseColor: baseColor, // Solid color
+        lightColor: shadeColor(baseColor, 0.5) // Lighter shade
+    };
+}
+
+// Function to lighten a color
+function shadeColor(color, percent) {
+    let r = parseInt(color.substring(1, 3), 16);
+    let g = parseInt(color.substring(3, 5), 16);
+    let b = parseInt(color.substring(5, 7), 16);
+
+    r = Math.min(255, Math.floor(r + (255 - r) * percent));
+    g = Math.min(255, Math.floor(g + (255 - g) * percent));
+    b = Math.min(255, Math.floor(b + (255 - b) * percent));
+
+    return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+}
